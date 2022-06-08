059cbe67ea5ec1fba98924042593d29fbde8bfe6
CaseyGann <cmoden@gannacademy.org>
2022-06-08 08:56:49 -0400

    major

--- src/main/java/com/example/Team.java
+++ src/main/java/com/example/Team.java
@@ -1,5 +1,63 @@
 package com.example;
 
+import java.util.ArrayList;
+
 public class Team {
+    private int size;
+
+
+    
+/* * * * * Beginning of Quiz * * * * */
+
+    /**
+     * Construct a new Team that has no Players on it, but is ready
+     * to start adding players.
+     */
+
+    public Team(int size) {
+        this.size = size;
+    }
+
+    
+    /**
+     * Add the given player to this team if there is room (i.e. the team is not at max size).
+     * @param player Player to add
+     */
+    public void addPlayer(Player player){
+
+    }
+
+    /**
+     * Remove the given Player from the team.
+     * @param player player to remove
+     */
+    public void removePlayer(Player player){
+
+    }
+    /**
+     * Return an <code>ArrayList</code> containing all players in this
+     * <code>Team</code> that match the given player.
+     * 
+     * @param template template to match to
+     * @return An <code>ArrayList</code> of all matching players.
+     */
+    public ArrayList<Player> getPlayers(Player template){
+        return null;
+    }
+
+
+    /**
+     * Return the first player in this <code>Team</code> that matches the
+     * given template, or <code>null</code> if there are no matching players.
+     * 
+     * @param template template to match to
+     * @return The first matching player, or <code>null</code> if there are
+     *          no matching players in this <code>Team</code>.
+     */
+    public Player getPlayer(Player player){
+        return null;
+    }
+
+    
     
 }
--- src/test/java/com/example/TestCases.java
+++ src/test/java/com/example/TestCases.java
@@ -7,14 +7,80 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class TestCases 
-{
+public class TestCases {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testMatches() {
+        Player fixedPlayer = new Player(3, "A", "B");
+        Player differentObjects = new Player(3, new String("A"), new String("B"));
+        Player differentName = new Player(3, "B", "B");
+        Player differentPosition = new Player(3, "A", "C");
+        Player differentNumber = new Player(2, "A", "B");
+        Player anyNumber = new Player(-1, "A", "B");
+        Player anyName = new Player(3, null, "B");
+        Player anyPosition = new Player(3, "A", null);
+        Player any = new Player(-1, null, null);
+
+        // Test constructor, getters, and setters
+        // I already wrote these; this is to make sure you didn't do something
+        // that would stop them from working.
+        assertTrue("The getNumber() method should return the number that was passed to the constructor.",
+                fixedPlayer.getNumber() == 3);
+        assertTrue("The getLastName() method should return the last name that was passed to the constructor.",
+                ("A").equals(fixedPlayer.getLastName()));
+        assertTrue("The getPosition() method should return the position that was passed to the constructor.",
+                ("B").equals(fixedPlayer.getPosition()));
+
+        // Test matches()
+        assertTrue("A player should always match itself.", fixedPlayer.matches(fixedPlayer));
+
+        assertTrue("Even if my last name and position are different String objects, they should match if they contain the same value. Are you comparing them with == rather than .equals()?", fixedPlayer.matches(differentObjects));
+
+        
+
+        assertFalse("A player should not match a player with different action.",
+                fixedLocation.matches(differentAction));
+
+        assertFalse("A player should not match a player with different x.",
+                fixedLocation.matches(differentX));
+
+        assertFalse("A player should not match a player with different y.",
+                fixedLocation.matches(differentY));
+
+        assertTrue("An x of -1 should match any x.",
+                fixedLocation.matches(anyX));
+
+        assertTrue("A y of -1 should match any y.",
+                fixedLocation.matches(anyY));
+
+        assertTrue(
+                "If one player matches any x and another matches any y, and their arguments match, the two should match.",
+                anyX.matches(anyY));
+
+        assertTrue(
+                "If one player matches any action and another matches any argument, and their x and y match, the two should match.",
+                anyArg.matches(anyAction));
+
+        assertFalse(
+                "Even if null or -1 are present in other characteristics, any characteristic that fails (x, in this case) should mean that the two don't match.",
+                anyY.matches(anyArg));
+
+        assertTrue("A Player with (-1, null, null) should match any player.",
+                any.matches(fixedLocation) && any.matches(differentArgs) && any.matches(differentAction) &&
+                        any.matches(differentX) && any.matches(differentY) && any.matches(anyY) && any.matches(anyX) &&
+                        any.matches(anyArg) && any.matches(anyAction));
+
+    }
+
+    @Test
+    public void testSearching() {
+
+    }
+
+    @Test
+    public void testAddAndRemove() {
+
     }
 }
